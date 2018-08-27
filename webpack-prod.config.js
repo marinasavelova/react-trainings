a4761d91342934e0103932e01f9e083d9ab268b1
Marina Korolenko <marina.savelova@pkp.vn.ua>
2018-08-27 17:17:20 +0300

    Lesson1 - Initial setup

---
+++ webpack-prod.config.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(common, {
+	mode: 'production',
+	devtool: 'source-map',
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				test: /\.js($|\?)/i,
+				sourceMap: true
+			})
+		]
+	}
+});
